0a035fc386c6018d0c30b134855bbd89e6f3de22
Santiago Franco <ktns0930@gmail.com>
2021-12-17 09:25:32 -0500

    CRUD is ready.

--- src/main/java/net/javaguides/springbootbackend/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springbootbackend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package net.javaguides.springbootbackend.controller;
 
+import net.javaguides.springbootbackend.exception.ResourceNotFoundException;
 import net.javaguides.springbootbackend.model.Employee;
 import net.javaguides.springbootbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,5 +25,33 @@ public class EmployeeController {
         return employeeRepository.save(employee);
     }
 
+    @DeleteMapping
+    public void deleteAllEmployees(){ employeeRepository.deleteAll(); }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+        Employee updateEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+
+        updateEmployee.setFirstName(employeeDetails.getFirstName());
+        updateEmployee.setLastName(employeeDetails.getLastName());
+        updateEmployee.setEmailId(employeeDetails.getEmailId());
+
+        employeeRepository.save(updateEmployee);
+
+        return ResponseEntity.ok(updateEmployee);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteEmployeeById(@PathVariable Long id){
+                employeeRepository.deleteById(id)   ;
+    }
 
 }
